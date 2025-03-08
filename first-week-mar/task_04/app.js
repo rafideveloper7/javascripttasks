99e1ec58105c5e8e440fcd83ec4ac567323b49db
Rafi Ullah <rafideveloper7@gmail.com>
2025-03-07 22:55:09 -0800

    updated task 4

--- first-week-mar/task_04/app.js
+++
@@ -1,5 +0,0 @@
-let arr = ["Red", "Green", "Blue"];
-console.log("Array before removing element :", arr)
-arr.pop();
-console.log( "after removing element : ",arr)
-console.log("printing after removed last elem : ",arr[2])
